1cc6acc0cde84a7a428897fafe56aef23dd1eb84
Daryn <dhaffer1991@gmail.com>
2021-10-25 11:25:39 -0500

    Doubly Linked Section

--- src/ds/doublylinkedlist/DoublyLinkedList.java
+++ src/ds/doublylinkedlist/DoublyLinkedList.java
@@ -52,8 +52,63 @@ public class DoublyLinkedList {
         return temp; // return the deleted old first node
     }
 
-    //assume non empty list
+    // assume non empty list
     public Node deleteLast(){
+        Node temp = last;
+        if(first.next == null){ // we only have one node in this list
+            first = null;
+        } else{
+            last.previous.next = null; // the last node's previous node's next field will point to null
+        }
+        last = last.previous;
+        return temp;
+    }
+
+    // assume a non-empty list
+    public boolean insertAfter(int key, int data) {
+        Node current = first; // start from the beginning of the list to start searching
+        while(current.data != key){ // as long as we have not found the key in a particular node
+            current = current.next;
+            if(current == null) {
+                return false;
+            }
+        }
+        Node newNode = new Node();
+        newNode.data = data;
+
+        if(current == last){
+            current.next = null;
+            last = newNode;
+        } else{
+            newNode.next = current.next; // new node's next field should point to the node ahead of current
+            current.next.previous = newNode;
+        }
+        newNode.previous = current;
+        current.next = newNode;
+
+        return true;
+
+    }
+
+    // assume non empty list
+    public Node deleteKey(int key){
+        Node current = first; // start from the beginning
+        while(current.data != key){
+            current = current.next;
+            if(current == null){
+                return null;
+            }
+        }
+
+        if (current == first) {
+            first = current.next; // make the first field point to the node following current since current will be deleted
+        } else{
+            current.previous.next = current.next;
+        }
+
+        if (current == last){
+            last = current.previous;
+        }
 
     }
 
